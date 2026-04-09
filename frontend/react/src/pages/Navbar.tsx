e73bc77803a1128890882d5d2a6137b298f31a07
PranavBharathi2809 <m.pranavbharathi@gmail.com>
2026-04-09 16:24:46 +0530

    Final code

---
+++ frontend/react/src/pages/Navbar.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import "../features/offer-letter/styles/Navbar.css"
+
+const Navbar: React.FC = () => {
+  return (
+    <div className="navbar">
+      {/* Left - Logo */}
+      <div className="nav-left">
+        <img
+         src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRpFWU9q3tLqaeQFb7msaTeRzww-PdoAVAlCg&s"
+          alt="Mirafra Logo"
+          className="logo"
+        />
+      </div>
+
+      {/* Center - Title */}
+      <div className="nav-center">
+        <h2>Offer Letter Generator</h2>
+      </div>
+
+      {/* Right (optional empty for balance) */}
+      <div className="nav-right"></div>
+    </div>
+  );
+};
+
+export default Navbar;
